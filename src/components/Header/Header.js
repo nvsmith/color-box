32e477804b9c99622745ff60907707cd2cf0fd5d
Nathan Smith <nathanielvs@gmail.com>
2024-05-21 08:45:38 -0600

    Add an app description

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,17 @@ const Header = () => {
     return (
         <header>
             <h1 id="title">Color Box</h1>
+            <p id="description">
+                A color swatch app that uses the{" "}
+                <a
+                    href="https://www.npmjs.com/package/colornames"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    colornames API
+                </a>
+                .
+            </p>
         </header>
     );
 };
